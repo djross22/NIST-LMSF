741f5195dec42354875cbff3675f4a23fbfb019e
David Ross <david.ross@nist.gov>
2019-01-13 15:33:59 -0500

    Overlord Simulator: Add open file from args[0] and display xml contenst of ovp file in ovpXmlDisplayBox

--- Overlord Simulator/App.xaml.cs
+++ Overlord Simulator/App.xaml.cs
@@ -13,5 +13,17 @@ namespace Overlord_Simulator
     /// </summary>
     public partial class App : Application
     {
+        public static string[] commandLineArgs;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // If no command line arguments were provided, don't process them 
+            if (e.Args.Length == 0) return;
+
+            if (e.Args.Length > 0)
+            {
+                commandLineArgs = e.Args;
+            }
+        }
     }
 }
--- Overlord Simulator/MainWindow.xaml.cs
+++ Overlord Simulator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,45 @@ namespace Overlord_Simulator
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] args = App.commandLineArgs;
+
+            if (App.commandLineArgs.Length > 0)
+            {
+                // write the ovp file name to the ovp path text box
+                ovpPathBox.Text = args[0];
+
+                try
+                {
+                    // Open the ovp file using a stream reader. 
+                    using (StreamReader sr = new StreamReader(args[0]))
+                    {
+                        // Read the stream to a string, and write  
+                        // the string to the ovp xml display text box 
+                        String line = sr.ReadToEnd();
+                        ovpXmlDisplayBox.AppendText(line.ToString());
+                        ovpXmlDisplayBox.AppendText("\n");
+                        
+                    }
+                }
+                catch (Exception e)
+                {
+                    ovpXmlDisplayBox.AppendText("The Overlord procedure file, ");
+                    ovpXmlDisplayBox.AppendText(args[0]);
+                    ovpXmlDisplayBox.AppendText(", could not be read:");
+                    ovpXmlDisplayBox.AppendText("\n");
+                    ovpXmlDisplayBox.AppendText(e.Message);
+                    ovpXmlDisplayBox.Foreground = Brushes.Red;
+                }
+                
+
+                foreach (string s in args)
+                {
+
+                }
+            }
+
+            
         }
     }
 }
