71f0a05b96ee8ea42e934cfc4ec7894abb3ccab0
David Ross <david.ross@nist.gov>
2019-01-15 08:30:15 -0500

    LMSF Scheduler: Add AutomationStep class to hold info about each automation step

---
+++ LMSF Scheduler/AutomationStep.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace LMSF_Scheduler
+{
+    public class AutomationStep : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string stepType;
+        private string stepShortDetail;
+        private bool isWaitUntil;
+        private bool waitCheckBoxEnabled;
+
+
+        #region Properties Getters and Setters
+        public string StepType
+        {
+            get { return this.stepType; }
+            set
+            {
+                this.stepType = value;
+                OnPropertyChanged("StepType");
+            }
+        }
+
+        public string StepShortDetail
+        {
+            get { return this.stepShortDetail; }
+            set
+            {
+                this.stepShortDetail = value;
+                OnPropertyChanged("StepShortDetail");
+            }
+        }
+
+        public bool IsWaitUntil
+        {
+            get { return this.isWaitUntil; }
+            set
+            {
+                this.isWaitUntil = value;
+                OnPropertyChanged("IsWaitUntil");
+            }
+        }
+
+        public bool WaitCheckBoxEnabled
+        {
+            get { return this.waitCheckBoxEnabled; }
+            set
+            {
+                this.waitCheckBoxEnabled = value;
+                OnPropertyChanged("WaitCheckBoxEnabled");
+            }
+        }
+        #endregion
+
+        public AutomationStep(string stepType, string stepShortDetail, bool isWaitUntil, bool waitCheckBoxEnabled)
+        {
+            this.stepType = stepType;
+            this.stepShortDetail = stepShortDetail;
+            this.isWaitUntil = isWaitUntil;
+            this.waitCheckBoxEnabled = waitCheckBoxEnabled;
+        }
+
+
+
+        public AutomationStep(string stepType)
+        {
+            this.stepType = stepType;
+            this.stepShortDetail = "short detail test";
+            this.isWaitUntil = true;
+            this.waitCheckBoxEnabled = false;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+    }
+}
