8f126e91ddf606f5da58de79766c6f99d918b511
David Ross <david.ross@nist.gov>
2019-01-15 08:31:51 -0500

    LMSF Scheduler: Add ListOfSteps ObservableCollection and have it displayed in the ListBox with a DataTemplate

--- LMSF Scheduler/MainWindow.xaml.cs
+++ LMSF Scheduler/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,24 @@ namespace LMSF_Scheduler
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<AutomationStep> ListOfSteps { get; set; }
+
         public MainWindow()
         {
+            ListOfSteps = new ObservableCollection<AutomationStep>();
+            //Add temporary list items for testing
+            TempStepInit();
+
             InitializeComponent();
+            DataContext = this;
+        }
+
+        //temporary method for debugging/testing
+        void TempStepInit()
+        {
+            ListOfSteps.Add(new AutomationStep("type 1"));
+            ListOfSteps.Add(new AutomationStep("type 2"));
+            ListOfSteps.Add(new AutomationStep("type 3"));
         }
     }
 }
