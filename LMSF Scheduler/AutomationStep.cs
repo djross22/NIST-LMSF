84300bf46126d3835fa30d3360a46a6b8764f179
David Ross <david.ross@nist.gov>
2019-01-15 09:32:08 -0500

    LMSF Scheduler: Add StepIcon as property for AutomationStep class and display icon in ListBox w/ DataTemplate

--- LMSF Scheduler/AutomationStep.cs
+++ LMSF Scheduler/AutomationStep.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Windows.Media.Imaging;
 
 namespace LMSF_Scheduler
 {
@@ -15,7 +16,11 @@ namespace LMSF_Scheduler
         private string stepShortDetail;
         private bool isWaitUntil;
         private bool waitCheckBoxEnabled;
+        private BitmapImage stepIcon;
 
+        //Icons for different step types
+        private static BitmapImage overlordIcon = new BitmapImage();
+        
 
         #region Properties Getters and Setters
         public string StepType
@@ -57,24 +62,43 @@ namespace LMSF_Scheduler
                 OnPropertyChanged("WaitCheckBoxEnabled");
             }
         }
+
+        public BitmapImage StepIcon
+        {
+            get { return this.stepIcon; }
+            set
+            {
+                this.stepIcon = value;
+                OnPropertyChanged("StepIcon");
+            }
+        }
         #endregion
 
         public AutomationStep(string stepType, string stepShortDetail, bool isWaitUntil, bool waitCheckBoxEnabled)
         {
+            InitIcons();
             this.stepType = stepType;
             this.stepShortDetail = stepShortDetail;
             this.isWaitUntil = isWaitUntil;
             this.waitCheckBoxEnabled = waitCheckBoxEnabled;
         }
 
-
-
         public AutomationStep(string stepType)
         {
+            InitIcons();
             this.stepType = stepType;
             this.stepShortDetail = "short detail test";
             this.isWaitUntil = true;
             this.waitCheckBoxEnabled = false;
+            this.stepIcon = overlordIcon;
+        }
+
+        void InitIcons()
+        {
+            overlordIcon = (BitmapImage)App.Current.Resources["Overlord BMP"];
+            //overlordIcon.BeginInit();
+            //overlordIcon.UriSource = new Uri("c:\\plus.png");
+            //overlordIcon.EndInit();
         }
 
         protected void OnPropertyChanged(string name)
