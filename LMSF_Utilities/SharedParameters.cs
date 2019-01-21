83fa895a8b56d69d3e730e5439a178398f589ab9
David Ross <david.ross@nist.gov>
2019-01-20 19:14:58 -0500

    LMSF_Utilities: Add SharedParameters Class for storage of constants used by Utility methods

---
+++ LMSF_Utilities/SharedParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSF_Utilities
+{
+    public static class SharedParameters
+    {
+        //Delimeter used in output files
+        public static string Delimeter => ",";
+
+        //Folders for shared data and metadata
+        public static string WorklistFolderPath => "C:\\Shared Files\\Data\\";
+        public static string MetadataFolderPath => "C:\\Shared Files\\MetaData Schema\\";
+        public static string OverlordFolderPath => "C:\\Shared Files\\Overlord-Venus\\";
+
+        //Media parameters
+        public static string MediaFolderPath => MetadataFolderPath + "MediaList\\";
+        public static string MediaFilePath => MediaFolderPath + "MediaList.csv";
+
+        //Strain parameters
+        public static string StrainFolderPath => MetadataFolderPath + "StrainList\\";
+        public static string StrainFilePath => StrainFolderPath + "StrainList.csv";
+
+        //Plasmid parameters
+        public static string PlasmidFolderPath => MetadataFolderPath + "PlasmidList\\";
+        public static string PlasmidFilePath => PlasmidFolderPath + "PlasmidList.csv";
+
+        //Additive parameters
+        public static string AdditiveFolderPath => MetadataFolderPath + "AdditiveList\\";
+        public static string AdditiveFilePath => AdditiveFolderPath + "AdditiveList.csv";
+
+        //Antibiotic parameters
+        public static string AntibioticFolderPath => MetadataFolderPath + "AntibioticList\\";
+        public static string AntibioticFilePath => AntibioticFolderPath + "AntibioticList.csv";
+
+        //Units
+        public static ObservableCollection<string> UnitsList => new ObservableCollection<string>() { "mmol/L", "umol/L", "mg/mL", "ug/mL", "ug/L", "%" };
+
+    }
+}
