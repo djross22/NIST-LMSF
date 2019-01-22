6d337a3672a3534e604a69df35932a8665127357
David Ross <david.ross@nist.gov>
2019-01-22 13:20:13 -0500

    LMSF_Utilities: Add ConcentrationDialog class

---
+++ LMSF_Utilities/ConcentrationDialog.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace LMSF_Utilities
+{
+    /// <summary>
+    /// Interaction logic for SelectMetaIdentDialog.xaml
+    /// </summary>
+    /// 
+    public partial class ConcentrationDialog : Window, INotifyPropertyChanged
+    {
+        //Property change notification event required for INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Note that ItemList handles notifications on its own (included with ObservableCollection class)
+        //    But the other fields have Property wrappers with set methods that handle the notification.
+        //    This is necessary to get data bindings to work properly with the GUI
+        public ObservableCollection<string> UnitsList { get; set; }
+        private string promptText;
+        private string ingredientName;
+        //in this dialog, the concentration is a string, making sure it is a valid number is handled by the NumberValidationRule
+        private string concValue;
+        private string selectedUnits;
+
+        #region Properties Getters and Setters
+        public string SelectedUnits
+        {
+            get { return this.selectedUnits; }
+            set
+            {
+                this.selectedUnits = value;
+                OnPropertyChanged("SelectedUnits");
+            }
+        }
+
+        public string ConcValue
+        {
+            get { return this.concValue; }
+            set
+            {
+                this.concValue = value;
+                OnPropertyChanged("ConcValue");
+            }
+        }
+
+        public string PromptText
+        {
+            get { return this.promptText; }
+            set
+            {
+                this.promptText = value;
+                OnPropertyChanged("PromptText");
+            }
+        }
+
+        public string IngredientName
+        {
+            get { return this.ingredientName; }
+            set
+            {
+                this.ingredientName = value;
+                OnPropertyChanged("IngredientName");
+            }
+        }
+        #endregion
+
+        public ConcentrationDialog()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            IngredientName = "additive";
+            PromptText = "Enter concentration and units for :" + IngredientName;
+            Title = "Concentration?";
+
+            UnitsList = SharedParameters.UnitsList;
+        }
+
+        public ConcentrationDialog(string name)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            IngredientName = name;
+            PromptText = "Enter concentration and units for :" + IngredientName;
+            Title = "Concentration?";
+
+            UnitsList = SharedParameters.UnitsList;
+        }
+
+        public ConcentrationDialog(string name, string prompt, string title)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            IngredientName = name;
+            PromptText = prompt;
+            Title = title;
+
+            UnitsList = SharedParameters.UnitsList;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+        
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SharedParameters.IsValid(this) && !(ConcValue is null))
+            {
+                this.DialogResult = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
