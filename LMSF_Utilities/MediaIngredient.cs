69854ec9cc3c6c62a6de4fd16db8c5a604d02b34
David Ross <david.ross@nist.gov>
2019-01-22 12:34:07 -0500

    LMSF_Utilities: Add Concentration class (value, units); edit button layout in MediaIngredientsDialog;

--- LMSF_Utilities/MediaIngredient.cs
+++ LMSF_Utilities/MediaIngredient.cs
@@ -9,34 +9,57 @@ namespace LMSF_Utilities
     public class MediaIngredient
     {
         public string Name { get; set; }
-        public double Concentration { get; set; }
-        public string Units { get; set; }
+        public Concentration Conc { get; set; }
 
         public MediaIngredient(string name, double conc, string units)
         {
             Name = name;
-            Concentration = conc;
-            Units = units;
+            Conc = new Concentration(conc, units);
         }
 
+        public MediaIngredient(string name, Concentration conc)
+        {
+            Name = name;
+            Conc = conc;
+        }
+
+        //This ToString method is aimed at formating the MediaIngredient for listing in a ListBox or other list control
+        //    the extra spaces added after the "%" units are a kludge to get the formatting nice without adjusting the tab stop positions in the ListBox
         public override string ToString()
         {
-            string units = Units;
+            string units = Conc.Units;
             if (units=="%")
             {
                 units = "%        ";
             }
-            return $"{Concentration} {units} \t{Name}";
+            return $"{Conc.Value} \t{units} \t\t{Name}";
         }
 
         public string SaveString()
         {
-            return $"{Name},{Concentration},{Units}";
+            return $"{Name},{Conc.Value},{Conc.Units}";
         }
 
         public static string HeaderString()
         {
             return "Ingredient,Concentration,Units";
+        }
+    }
+
+    public class Concentration
+    {
+        public double Value { get; set; }
+        public string Units { get; set; }
+
+        public Concentration(double value, string units)
+        {
+            Value = value;
+            Units = units;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} {Units}";
         }
     }
 }
