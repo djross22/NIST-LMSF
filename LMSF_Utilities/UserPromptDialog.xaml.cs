802906cff0b144488dc94a99ca33b253d223802e
David Ross <david.ross@nist.gov>
2019-01-22 19:14:33 -0500

    LMSF_Utilities: Add UserPrompt dialog box

---
+++ LMSF_Utilities/UserPromptDialog.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace LMSF_Utilities
+{
+    /// <summary>
+    /// Interaction logic for SelectMetaIdentDialog.xaml
+    /// </summary>
+    /// 
+    public partial class UserPromptDialog : Window, INotifyPropertyChanged
+    {
+        //Property change notification event required for INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string promptText;
+
+        #region Properties Getters and Setters
+        public string PromptText
+        {
+            get { return this.promptText; }
+            set
+            {
+                this.promptText = value;
+                OnPropertyChanged("PromptText");
+            }
+        }
+        #endregion
+
+        public UserPromptDialog()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public UserPromptDialog(string prompt)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            PromptText = prompt;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
