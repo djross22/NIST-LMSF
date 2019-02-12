7aac5e48798607a97d98091c0677e25d1cf3f0dc
David Ross <david.ross@nist.gov>
2019-02-12 12:31:37 -0500

    Add new projects for creating Gen5 reader: LMSF_Gen5_Reader.dll and LMSF_Gen5.exe (WFP Window)

---
+++ LMSF_Gen5_Reader/Gen5Reader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSF_Gen5_Reader
+{
+    public class Gen5Reader
+    {
+    }
+}
