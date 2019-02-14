2131e5de2f4dd9f2ff9d8ee9c2eaaa9f07204829
David Ross <david.ross@nist.gov>
2019-02-13 19:37:51 -0500

    Add IReaderTextOut interface to use for sending text responses back to calling .exe from LMSF_Gen5_Reader.dll

---
+++ LMSF_Gen5_Reader/IReaderTextOut.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSF_Gen5_Reader
+{
+    public interface IReaderTextOut
+    {
+        string TextOut { get; set; }
+    }
+}
