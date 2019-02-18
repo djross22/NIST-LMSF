009c9267cf73e377f3235adbc95d1e9de675c88a
David Ross <david.ross@nist.gov>
2019-02-18 16:08:36 -0500

    adjust output text formatting

--- LMSF Scheduler/MainWindow.xaml.cs
+++ LMSF Scheduler/MainWindow.xaml.cs
@@ -3708,7 +3708,7 @@ namespace LMSF_Scheduler
             //wrappedMessage += "1";
             //Note WriteLineAndGetReply() returns null if server takes longer than timeout to send reply
             Message replyMsg = null;
-            AddOutputText($"sending message to reader, {wrappedMessage}... ");
+            AddOutputText($"sending message to reader, {wrappedMessage} ... ");
             //TODO: add maxRetries
             int numTries = 0;
             while (replyMsg == null)
