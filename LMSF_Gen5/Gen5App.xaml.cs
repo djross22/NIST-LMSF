53812302305b57a8eceb674f3236375a24c8ccf9
David Ross <david.ross@nist.gov>
2019-02-20 15:37:22 -0500

    Make LMSF_Gen5 single instance

--- LMSF_Gen5/Gen5App.xaml.cs
+++ LMSF_Gen5/Gen5App.xaml.cs
@@ -5,13 +5,35 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using LMSF_Utilities;
 
 namespace LMSF_Gen5
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        //Code from: https://www.codeproject.com/Articles/84270/WPF-Single-Instance-Application
+        private const string Unique = "LMSF_Gen5_Unique_Hold_String";
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments of second instance
+            // ...
+            return true;
+        }
     }
 }
